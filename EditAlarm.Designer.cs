402fa05868cd78275f21b63528094b05672d8d7a
Jonas Smith <pictofoster@gmail.com>
2018-02-02 15:14:47 -0600

    Documentation & styling for my DGV.

--- EditAlarm.Designer.cs
+++ EditAlarm.Designer.cs
@@ -30,6 +30,8 @@
     {
       this.components = new System.ComponentModel.Container();
       System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+      System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+      System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
       this.msmMain = new MetroFramework.Components.MetroStyleManager(this.components);
       this.metroLink1 = new MetroFramework.Controls.MetroLink();
       this.bgWorker = new System.ComponentModel.BackgroundWorker();
@@ -56,34 +58,48 @@
       this.alarmDGV.AllowUserToDeleteRows = false;
       this.alarmDGV.AllowUserToResizeColumns = false;
       this.alarmDGV.AllowUserToResizeRows = false;
+      this.alarmDGV.BorderStyle = System.Windows.Forms.BorderStyle.None;
       dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-      dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+      dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
       dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      dataGridViewCellStyle1.ForeColor = System.Drawing.Color.PaleGreen;
-      dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
-      dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.GrayText;
+      dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Lime;
+      dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+      dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Lime;
       dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
       this.alarmDGV.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
       this.alarmDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
       this.alarmDGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.alarmName,
             this.alarmTime});
+      this.alarmDGV.Dock = System.Windows.Forms.DockStyle.Fill;
       this.alarmDGV.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
-      this.alarmDGV.Location = new System.Drawing.Point(12, 63);
+      this.alarmDGV.Location = new System.Drawing.Point(20, 60);
       this.alarmDGV.Name = "alarmDGV";
       this.alarmDGV.ReadOnly = true;
       this.alarmDGV.RowHeadersVisible = false;
-      this.alarmDGV.Size = new System.Drawing.Size(425, 275);
+      this.alarmDGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+      this.alarmDGV.Size = new System.Drawing.Size(410, 270);
       this.alarmDGV.TabIndex = 0;
       // 
       // alarmName
       // 
+      dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+      dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      dataGridViewCellStyle2.ForeColor = System.Drawing.Color.ForestGreen;
+      dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.DarkSlateGray;
+      dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Lime;
+      this.alarmName.DefaultCellStyle = dataGridViewCellStyle2;
       this.alarmName.HeaderText = "Alarm Name";
       this.alarmName.Name = "alarmName";
       this.alarmName.ReadOnly = true;
       // 
       // alarmTime
       // 
+      dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+      dataGridViewCellStyle3.ForeColor = System.Drawing.Color.ForestGreen;
+      dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.DarkSlateGray;
+      dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Lime;
+      this.alarmTime.DefaultCellStyle = dataGridViewCellStyle3;
       this.alarmTime.HeaderText = "Alarm Time";
       this.alarmTime.Name = "alarmTime";
       this.alarmTime.ReadOnly = true;
--- EditAlarm.cs
+++ EditAlarm.cs
@@ -10,12 +10,12 @@ namespace SpotiAlarm
 {
   public partial class EditAlarm : MetroFramework.Forms.MetroForm
   {
-    public  int                 hh;
-    public  int                 mm;
-    public  int                 ss;
-    public  int                 repeat;
-    public  string              reCheck;
-    private static Alarm        alarm;
+    public  int                hh;
+    public  int                mm;
+    public  int                ss;
+    public  int                repeat;
+    public  string             reCheck;
+    private static Alarm       alarm;
     private static List<Alarm> alarmListTemp;
 
     public EditAlarm()
@@ -28,19 +28,26 @@ namespace SpotiAlarm
       // <Summary>
       //    loads "dark" style to match spotify 
       // </Summary>
+      #region MetroTheme
       this.StyleManager = msmMain;
       msmMain.Theme     = MetroFramework.MetroThemeStyle.Dark;
       msmMain.Style     = MetroFramework.MetroColorStyle.Green;
+      #endregion
 
       alarmListTemp = (List<Alarm>)this.Tag;
 
       LoadDGV();
-
     }
 
     private void LoadDGV()
     {
-      for(int i = 1; i <= alarmListTemp.Count; i++)
+      alarmDGV.EnableHeadersVisualStyles = false;
+      alarmDGV.ColumnHeadersDefaultCellStyle.BackColor = Color.DarkSlateGray;
+      alarmDGV.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+      alarmDGV.Columns[0].HeaderCell.Style.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+      alarmDGV.Columns[1].HeaderCell.Style.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+
+      for (int i = 1; i <= alarmListTemp.Count; i++)
       {
         alarmDGV.Rows.Add();
         alarmDGV.Rows[i - 1].Cells[0].Value = alarmListTemp[i - 1].Name;
